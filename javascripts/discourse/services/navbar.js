2d9e952bdd3b9e07c437f1e487db1dddec32c387
Kris <kris.aubuchon@discourse.org>
2023-12-08 13:33:54 -0500

    REFACTOR: restyle theme, add header and footer (#5)

---
+++ javascripts/discourse/services/navbar.js
@@ -0,0 +1,21 @@
+import Service from "@ember/service";
+
+export default Service.extend({
+  init() {
+    this._super(...arguments);
+
+    this.items = [];
+  },
+
+  register(item) {
+    this.items.push(item);
+  },
+
+  closePopupMenu(menu) {
+    this.items.forEach((item) => {
+      if (item !== menu) {
+        item.closeDropdown();
+      }
+    });
+  },
+});
